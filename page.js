d091a31a7835c7b37d455802f85048e9573b82fb
Julia Tsai <42037818+YayaTsai@users.noreply.github.com>
2022-05-02 23:51:55 +0800

    修復文章 Name id 重複而無法正常顯示內容的問題

--- page.js
+++ page.js
@@ -12,14 +12,15 @@ function create_page(i){
 
 function insert_page_type1L(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3,stc1,stc2){
 
+	var text1_id = text1, text2_id = text2, text3_id = text3;
     requirejs([text1], function(text1) {
-        document.getElementById("Text1_L").innerHTML = text1.Text();
+        document.getElementById(text1_id).innerHTML = text1.Text();
     });
     requirejs([text2], function(text2) {
-        document.getElementById("Text2_L").innerHTML = text2.Text();
+        document.getElementById(text2_id).innerHTML = text2.Text();
     });
     requirejs([text3], function(text3) {
-        document.getElementById("Text3_L").innerHTML = text3.Text();
+        document.getElementById(text3_id).innerHTML = text3.Text();
     });
     
     $('.page').eq(i).append('\
@@ -38,13 +39,13 @@ function insert_page_type1L(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3
                 <div id="name2_L_type1" class="grid_name_type1">'+name2+'</div>\
                 <div id="name3_L_type1" class="grid_name_type1">'+name3+'</div>\
                 <div id="text1_L_type1" class="grid_block_type1">\
-                    <div id="Text1_L" class="grid_text_short_type1"></div>\
+                    <div id="'+text1_id+'" class="grid_text_short_type1"></div>\
                 </div>\
                 <div id="text2_L_type1" class="grid_block_type1">\
-                    <div id="Text2_L" class="grid_text_long_type1"></div>\
+                    <div id="'+text2_id+'" class="grid_text_long_type1"></div>\
                 </div>\
                 <div id="text3_L_type1" class="grid_block_type1">\
-                    <div id="Text3_L" class="grid_text_short_type1"></div>\
+                    <div id="'+text3_id+'" class="grid_text_short_type1"></div>\
                 </div>\
                 <img id="stc1_L_type1" src="'+stc1+'" class="grid_stc_pic_type1"></img>\
                 <img id="stc2_L_type1" src="'+stc2+'" class="grid_stc_pic_type1"></img>\
@@ -55,14 +56,15 @@ function insert_page_type1L(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3
 }
 function insert_page_type1R(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3,stc1,stc2){
 
+	var text1_id = text1, text2_id = text2, text3_id = text3;
     requirejs([text1], function(text1) {
-        document.getElementById("Text1_R").innerHTML = text1.Text();
+        document.getElementById(text1_id).innerHTML = text1.Text();
     });
     requirejs([text2], function(text2) {
-        document.getElementById("Text2_R").innerHTML = text2.Text();
+        document.getElementById(text2_id).innerHTML = text2.Text();
     });
     requirejs([text3], function(text3) {
-        document.getElementById("Text3_R").innerHTML = text3.Text();
+        document.getElementById(text3_id).innerHTML = text3.Text();
     });
     
     $('.page').eq(i).append('\
@@ -81,13 +83,13 @@ function insert_page_type1R(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3
                 <div id="name2_R_type1" class="grid_name_type1">'+name2+'</div>\
                 <div id="name3_R_type1" class="grid_name_type1">'+name3+'</div>\
                 <div id="text1_R_type1" class="grid_block_type1">\
-                    <div id="Text1_R" class="grid_text_long_type1"></div>\
+                    <div id="'+text1_id+'" class="grid_text_long_type1"></div>\
                 </div>\
                 <div id="text2_R_type1" class="grid_block_type1">\
-                    <div id="Text2_R" class="grid_text_short_type1"></div>\
+                    <div id="'+text2_id+'" class="grid_text_short_type1"></div>\
                 </div>\
                 <div id="text3_R_type1" class="grid_block_type1">\
-                    <div id="Text3_R" class="grid_text_short_type1"></div>\
+                    <div id="'+text3_id+'" class="grid_text_short_type1"></div>\
                 </div>\
                 <img id="stc1_R_type1" src="'+stc1+'" class="grid_stc_pic_type1"></img>\
                 <img id="stc2_R_type1" src="'+stc2+'" class="grid_stc_pic_type1"></img>\
