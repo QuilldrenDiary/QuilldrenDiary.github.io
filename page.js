2c88e17fc398683617735f9a5a013ddbf1354ca6
tsukiyo10884 <tsukiyo10884@gmail.com>
2022-05-19 23:36:35 +0800

    修正page的id部分

--- page.js
+++ page.js
@@ -37,31 +37,31 @@ function insert_page_type1L(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3
     });
     
     $('.page').eq(i).append('\
-        <div id="book_L_type1" class="grid">\
+        <div class="grid">\
             <div class="container_bookL_type1">\
-                <div id="pic1_L_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 pic1_L_type1">\
                     <img src="'+pic1+'" class="grid_pic_4_3_type1"></img>\
                 </div>\
-                <div id="pic2_L_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 pic2_L_type1">\
                     <img src="'+pic2+'" class="grid_pic_1_1_type1"></img>\
                 </div>\
-                <div id="pic3_L_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 pic3_L_type1">\
                     <img src="'+pic3+'" class="grid_pic_1_1_type1"></img>\
                 </div>\
-                <div id="name1_L_type1" class="grid_name_type1">'+name1+'</div>\
-                <div id="name2_L_type1" class="grid_name_type1">'+name2+'</div>\
-                <div id="name3_L_type1" class="grid_name_type1">'+name3+'</div>\
-                <div id="text1_L_type1" class="grid_block_type1">\
+                <div class="grid_name_type1 name1_L_type1">'+name1+'</div>\
+                <div class="grid_name_type1 name2_L_type1">'+name2+'</div>\
+                <div class="grid_name_type1 name3_L_type1">'+name3+'</div>\
+                <div class="grid_block_type1 text1_L_type1">\
                     <div id="'+text1_id+'" class="grid_text_short_type1"></div>\
                 </div>\
-                <div id="text2_L_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 text2_L_type1">\
                     <div id="'+text2_id+'" class="grid_text_long_type1"></div>\
                 </div>\
-                <div id="text3_L_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 text3_L_type1">\
                     <div id="'+text3_id+'" class="grid_text_short_type1"></div>\
                 </div>\
-                <img id="stc1_L_type1" src="'+stc1+'" class="grid_stc_pic_type1"></img>\
-                <img id="stc2_L_type1" src="'+stc2+'" class="grid_stc_pic_type1"></img>\
+                <img src="'+stc1+'" class="grid_stc_pic_type1 stc1_L_type1"></img>\
+                <img src="'+stc2+'" class="grid_stc_pic_type1 stc2_L_type1"></img>\
             </div>\
         </div>\
     ');
@@ -87,31 +87,31 @@ function insert_page_type1R(i,pic1,pic2,pic3,name1,name2,name3,text1,text2,text3
     });
     
     $('.page').eq(i).append('\
-        <div id="book_R_type1" class="grid">\
+        <div class="grid">\
             <div class="container_bookR_type1">\
-                <div id="pic1_R_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 pic1_R_type1">\
                     <img src="'+pic1+'" class="grid_pic_1_1_type1"></img>\
                 </div>\
-                <div id="pic2_R_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 pic2_R_type1">\
                     <img src="'+pic2+'" class="grid_pic_1_1_type1"></img>\
                 </div>\
-                <div id="pic3_R_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 pic3_R_type1">\
                     <img src="'+pic3+'" class="grid_pic_4_3_type1"></img>\
                 </div>\
-                <div id="name1_R_type1" class="grid_name_type1">'+name1+'</div>\
-                <div id="name2_R_type1" class="grid_name_type1">'+name2+'</div>\
-                <div id="name3_R_type1" class="grid_name_type1">'+name3+'</div>\
-                <div id="text1_R_type1" class="grid_block_type1">\
+                <div class="grid_name_type1 name1_R_type1">'+name1+'</div>\
+                <div class="grid_name_type1 name2_R_type1">'+name2+'</div>\
+                <div class="grid_name_type1 name3_R_type1">'+name3+'</div>\
+                <div class="grid_block_type1 text1_R_type1">\
                     <div id="'+text1_id+'" class="grid_text_long_type1"></div>\
                 </div>\
-                <div id="text2_R_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 text2_R_type1">\
                     <div id="'+text2_id+'" class="grid_text_short_type1"></div>\
                 </div>\
-                <div id="text3_R_type1" class="grid_block_type1">\
+                <div class="grid_block_type1 text3_R_type1">\
                     <div id="'+text3_id+'" class="grid_text_short_type1"></div>\
                 </div>\
-                <img id="stc1_R_type1" src="'+stc1+'" class="grid_stc_pic_type1"></img>\
-                <img id="stc2_R_type1" src="'+stc2+'" class="grid_stc_pic_type1"></img>\
+                <img src="'+stc1+'" class="grid_stc_pic_type1 stc1_R_type1"></img>\
+                <img src="'+stc2+'" class="grid_stc_pic_type1 stc2_R_type1"></img>\
             </div>\
         </div>\
     ');
@@ -126,28 +126,28 @@ function insert_page_type2L(i,pic1,pic2,pic3,pic4,pic5,name1,name2,name3,name4,n
     }
 
     $('.page').eq(i).append('\
-        <div id="book_L_type2" class="grid">\
+        <div class="grid">\
             <div class="container_bookL_type2">\
-                <div id="pic1_L_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic1_L_type2">\
                     <img src="'+pic1+'" class="grid_pic_4_3_type2"></img>\
                 </div>\
-                <div id="pic2_L_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic2_L_type2">\
                     <img src="'+pic2+'" class="grid_pic_4_3_type2"></img>\
                 </div>\
-                <div id="pic3_L_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic3_L_type2">\
                     <img src="'+pic3+'" class="grid_pic_4_3_type2"></img>\
                 </div>\
-                <div id="pic4_L_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic4_L_type2">\
                     <img src="'+pic4+'" class="grid_pic_1_1_type2"></img>\
                 </div>\
-                <div id="pic5_L_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic5_L_type2">\
                     <img src="'+pic5+'" class="grid_pic_1_1_type2"></img>\
                 </div>\
-                <div id="name1_L_type2" class="grid_name_type2">'+name1+'</div>\
-                <div id="name2_L_type2" class="grid_name_type2">'+name2+'</div>\
-                <div id="name3_L_type2" class="grid_name_type2">'+name3+'</div>\
-                <div id="name4_L_type2" class="grid_name_type2">'+name4+'</div>\
-                <div id="name5_L_type2" class="grid_name_type2">'+name5+'</div>\
+                <div class="grid_name_type2 name1_L_type2">'+name1+'</div>\
+                <div class="grid_name_type2 name2_L_type2">'+name2+'</div>\
+                <div class="grid_name_type2 name3_L_type2">'+name3+'</div>\
+                <div class="grid_name_type2 name4_L_type2">'+name4+'</div>\
+                <div class="grid_name_type2 name5_L_type2">'+name5+'</div>\
             </div>\
         </div>\
     ');
@@ -162,30 +162,40 @@ function insert_page_type2R(i,pic1,pic2,pic3,pic4,pic5,name1,name2,name3,name4,n
     }
 
     $('.page').eq(i).append('\
-        <div id="book_R_type2" class="grid">\
+        <div class="grid">\
             <div class="container_bookR_type2">\
-                <div id="pic1_R_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic1_R_type2">\
                     <img src="'+pic1+'" class="grid_pic_4_3_type2"></img>\
                 </div>\
-                <div id="pic2_R_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic2_R_type2">\
                     <img src="'+pic2+'" class="grid_pic_4_3_type2"></img>\
                 </div>\
-                <div id="pic3_R_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic3_R_type2">\
                     <img src="'+pic3+'" class="grid_pic_4_3_type2"></img>\
                 </div>\
-                <div id="pic4_R_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic4_R_type2">\
                     <img src="'+pic4+'" class="grid_pic_1_1_type2"></img>\
                 </div>\
-                <div id="pic5_R_type2" class="grid_block_type2">\
+                <div class="grid_block_type2 pic5_R_type2">\
                     <img src="'+pic5+'" class="grid_pic_1_1_type2"></img>\
                 </div>\
-                <div id="name1_R_type2" class="grid_name_type2">'+name1+'</div>\
-                <div id="name2_R_type2" class="grid_name_type2">'+name2+'</div>\
-                <div id="name3_R_type2" class="grid_name_type2">'+name3+'</div>\
-                <div id="name4_R_type2" class="grid_name_type2">'+name4+'</div>\
-                <div id="name5_R_type2" class="grid_name_type2">'+name5+'</div>\
+                <div class="grid_name_type2 name1_R_type2">'+name1+'</div>\
+                <div class="grid_name_type2 name2_R_type2">'+name2+'</div>\
+                <div class="grid_name_type2 name3_R_type2">'+name3+'</div>\
+                <div class="grid_name_type2 name4_R_type2">'+name4+'</div>\
+                <div class="grid_name_type2 name5_R_type2">'+name5+'</div>\
             </div>\
         </div>\
     ')
+
+}
+function insert_last_page(i){
+    if ($(window).width() > 480) {
+        i=i+2;
+    }else{
+        i=i*2+2;
+    }
+
+    $('.page').eq(i).append('<div id="end_paper"></div>')
 
 }
